295e1cb32b5b39554b5ee691320fcbd9b6849e42
DAMODAR VALLURU <damodarvalluru7@gmail.com>
2026-06-15 19:52:23 +0530

    some respective changes are done

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const twilio = require("twilio");
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/")));
 
 // 🔹 Email Setup
 const transporter = nodemailer.createTransport({
@@ -63,7 +63,7 @@ app.post("/send", async (req, res) => {
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname,"index.html"));
 });
 
 // 🔹 Start Server
